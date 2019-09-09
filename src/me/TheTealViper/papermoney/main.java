5a4b61be7e3c5497b1fb9a598bbe608bdb112f7a
TheTealViper <thetealviper@gmail.com>
2019-09-09 11:20:28 -0700

    [1.14.4.a.3] Bug fix

--- src/me/TheTealViper/papermoney/main.java
+++ src/me/TheTealViper/papermoney/main.java
@@ -310,30 +310,7 @@ public class main extends JavaPlugin implements Listener{
     }
    
     public static String makeColors(String s){
-        String replaced = s
-                .replaceAll("&0", "" + ChatColor.BLACK)
-                .replaceAll("&1", "" + ChatColor.DARK_BLUE)
-                .replaceAll("&2", "" + ChatColor.DARK_GREEN)
-                .replaceAll("&3", "" + ChatColor.DARK_AQUA)
-                .replaceAll("&4", "" + ChatColor.DARK_RED)
-                .replaceAll("&5", "" + ChatColor.DARK_PURPLE)
-                .replaceAll("&6", "" + ChatColor.GOLD)
-                .replaceAll("&7", "" + ChatColor.GRAY)
-                .replaceAll("&8", "" + ChatColor.DARK_GRAY)
-                .replaceAll("&9", "" + ChatColor.BLUE)
-                .replaceAll("&a", "" + ChatColor.GREEN)
-                .replaceAll("&b", "" + ChatColor.AQUA)
-                .replaceAll("&c", "" + ChatColor.RED)
-                .replaceAll("&d", "" + ChatColor.LIGHT_PURPLE)
-                .replaceAll("&e", "" + ChatColor.YELLOW)
-                .replaceAll("&f", "" + ChatColor.WHITE)
-                .replaceAll("&r", "" + ChatColor.RESET)
-                .replaceAll("&l", "" + ChatColor.BOLD)
-                .replaceAll("&o", "" + ChatColor.ITALIC)
-                .replaceAll("&k", "" + ChatColor.MAGIC)
-                .replaceAll("&m", "" + ChatColor.STRIKETHROUGH)
-                .replaceAll("&n", "" + ChatColor.UNDERLINE)
-                .replaceAll("\\\\", " ");
+        String replaced = ChatColor.translateAlternateColorCodes('&', s.replaceAll("\\\\", " "));
         return replaced;
     }
    
