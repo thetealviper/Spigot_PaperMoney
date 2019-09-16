ce9b26799f81a88a94b9b239df0ed67274921933
TheTealViper <thetealviper@gmail.com>
2019-09-16 12:52:32 -0700

    [1.14.4.a.4] Lava bug fix

--- src/me/TheTealViper/papermoney/holographShit.java
+++ src/me/TheTealViper/papermoney/holographShit.java
@@ -17,12 +17,17 @@ public class holographShit {
 		if(!plugin.getConfig().getBoolean("Use_Holograms_If_Possible"))
 			return;
 		if(!holographDatabase.containsKey(entityUuid)){
+			if(plugin.getServer().getEntity(entityUuid) == null) {
+				return;
+			}
 			Hologram holo = HologramsAPI.createHologram(plugin, plugin.getServer().getEntity(entityUuid).getLocation().clone().add(0, 1, 0));
 			holo.appendTextLine(ChatColor.GREEN + "$" + ChatColor.GOLD + plugin.amountMap.get(entityUuid));
 			holographDatabase.put(entityUuid, holo);
 		}else{
-			if(plugin.getServer().getEntity(entityUuid) == null)
+			if(plugin.getServer().getEntity(entityUuid) == null) {
+				holographDatabase.get(entityUuid).delete();
 				holographDatabase.remove(entityUuid);
+			}
 			else {
 				Hologram holo = holographDatabase.get(entityUuid);
 				holo.teleport(plugin.getServer().getEntity(entityUuid).getLocation().clone().add(0, 1, 0));
